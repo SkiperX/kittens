3fd14a87e75bb413258d9d216579b13732abfab4
Александр Рспономарев <sanya2128@gmail.com>
2018-02-12 14:05:02 +0500

    Добавляет файлы

---
+++ src/js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+
+    $(".portions__group").on("click", function(e) {
+        $(this).addClass("portions__group--clicked");
+    });
+
+    $('.portions__group').hover(
+        function(){
+        },
+        function(){
+            $('.portions__group').removeClass("portions__group--clicked");
+        });
+
+    $("body").keydown(function(e){
+        if(e.keyCode==9){
+            $("body").addClass("tab-user");
+        }
+    });
+    $("body").css("opacity", 1).addClass("body-ready");
+});
+$(window).load(function() {
+
+});
+
+
+
---
+++ src/js/vendors.js
@@ -0,0 +1,7 @@
+
+
+// ../../bower_components/lightgallery/dist/js/lightgallery-all.min.js
+// ../../bower_components/wow/dist/wow.min.js
+// ../sass/vendor/jQueryFormStyler/jquery.formstyler.min.js
+// ../sass/vendor/wow.min.js
+// ../sass/vendor/scrollax.min.js
